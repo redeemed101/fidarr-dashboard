afca05f7a286f690bf43f9e5779675c69e50b7e3
nbsdeveloper-lewis <75124231+nbsdeveloper-lewis@users.noreply.github.com>
2023-04-08 22:12:36 +0200

    Created playlist table

--- app/src/App.tsx
+++ app/src/App.tsx
@@ -1,17 +1,6 @@
 
 import './App.css';
-
-import RecoverPasswordPage from './Presentation/Authentication/Pages/RecoverPasswordPage';
-import DashboardMainPage from './Presentation/Dashboard/Pages/DashboardMainPage';
-import Status403Page from './Presentation/Errors/Pages/Status403Page';
-import AlbumsPage from './Presentation/Music/Pages/AlbumsPage';
-import ArtistsPage from './Presentation/Music/Pages/ArtistsPage';
-import GenresPage from './Presentation/Music/Pages/GenresPage';
 import PlaylistsPage from './Presentation/Music/Pages/PlaylistsPage';
-import TracksPage from './Presentation/Music/Pages/TracksPage';
-import AlbumsTable from './Presentation/Music/Sections/AlbumsTable';
-import ArtistsTable from './Presentation/Music/Sections/ArtistsTable';
-import MusicHeader from './Presentation/Music/Sections/MusicHeader';
 
 
 const title: string = "Login"
